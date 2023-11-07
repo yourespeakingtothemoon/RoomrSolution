6bf914e723fa19a5afd5db8df60a0804c50bfcf9
yourespeakingtothemoon <91557919+yourespeakingtothemoon@users.noreply.github.com>
2023-11-07 08:48:00 -0700

    Add project files.

---
+++ Roomr/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Roomr
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Roomr/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Roomr
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
