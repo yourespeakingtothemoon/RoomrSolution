c168ced48cc0e80daf438a412477edcbd51948d7
bsmithNeumont <brsmith@student.neumont.edu>
2023-11-15 09:02:19 -0700

    Added onclicks to signup page

--- Roomr/LoginPage.xaml.cs
+++ Roomr/LoginPage.xaml.cs
@@ -21,11 +21,5 @@ public partial class LoginPage : ContentPage
     private void OnSignUpClicked(object sender, EventArgs e)
 	{
         //Move to Sign Up Page
-
-        //In Sign Up Page:
-        //TODO: Add sign up form, create Person object with validated data, save it:
-
-        //Person person = new Person("David", "sampleUsername", "samplePass", "Phone number: 867-5309", "Salt Lake City", "Utah", "United States of America", "");
-        //await database.SavePersonAsync(person);
     }
 }
--- Roomr/SignUpPage.xaml.cs
+++ Roomr/SignUpPage.xaml.cs
@@ -7,4 +7,16 @@ public partial class SignUpPage : ContentPage
 		InitializeComponent();
 	}
 
+	public async void OnSignUpClicked(object sender, EventArgs e)
+	{
+        //TODO: Add sign up form, create Person object with validated data, save it:
+
+        //Person person = new Person("David", "sampleUsername", "samplePass", "Phone number: 867-5309", "Salt Lake City", "Utah", "United States of America", "");
+        //await database.SavePersonAsync(person);
+    }
+
+    public void OnLoginClicked(object sender, EventArgs e)
+	{
+		//Move to LoginPage
+	}
 }
