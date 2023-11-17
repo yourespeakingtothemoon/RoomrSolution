6a59a2341a9510b35b0be730621e1c05e0a34762
yourespeakingtothemoon <91557919+yourespeakingtothemoon@users.noreply.github.com>
2023-11-17 11:00:08 -0700

    Added profile functions, some should carry over into the profile card. Need to add contact info. Idk how we want to format quiet hours, hope this is good.

--- Roomr/ProfilePage.xaml.cs
+++ Roomr/ProfilePage.xaml.cs
@@ -1,7 +1,12 @@
+using Android.App;
+
 namespace Roomr;
 
 public partial class ProfilePage : ContentPage
 {
+
+
+
 	public ProfilePage()
 	{
 		InitializeComponent();
@@ -10,5 +15,61 @@ public partial class ProfilePage : ContentPage
     private void ScrollView_Scrolled(object sender, ScrolledEventArgs e)
     {
 	
+    }
+
+
+	private Person person;
+
+	private void HobbyList()
+	{
+		string hobbs = "";
+		//to do, get reference to hobbies list
+//	person.Hobbies
+List<string> list = new List<string>();
+		foreach (var item in list)
+		{
+			hobbs+= item + ", ";
+        }
+		hobbs = hobbs.Substring(0, hobbs.Length - 2);
+		hobbies.Text = hobbs;
+	}
+    private void ChoresList()
+    {
+        string chorz = "";
+        //to do, get reference to hobbies list
+        //	person.Hobbies
+        List<string> list = new List<string>();
+        foreach (var item in list)
+        {
+            chorz += item + ", ";
+        }
+        chorz = chorz.Substring(0, chorz.Length - 2);
+        chores.Text = chorz;
+    }
+    private void LocationAndDist()
+    {
+        //to do, get reference to location
+
+        //to do reference ethan's distance getter
+        string loc = "";
+        float dist = 0.0f;
+        distance.Text = dist + " UNITS";
+        location.Text = loc;
+       
+    }
+
+    private void QuietHours()
+    {
+        //to do get references to quiet hours
+        string qh = "";
+        int beginHour = 0;
+        int endHour = 0;
+        int beginMin = 0;
+        int endMin = 0;
+        //Or do we wanna round up to the nearest half hour with a float?
+
+        qh = beginHour + ":" + beginMin + " - " + endHour + ":" + endMin;
+        quietHours.Text = qh;
+        
     }
 }
