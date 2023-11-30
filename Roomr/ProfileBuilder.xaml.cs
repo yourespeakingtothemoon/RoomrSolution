26af9d83bd8aa835d227064498f2c6a3be5170a8
yourespeakingtothemoon <91557919+yourespeakingtothemoon@users.noreply.github.com>
2023-11-30 10:38:51 -0700

    imagebutton!

--- Roomr/ProfileBuilder.xaml.cs
+++ Roomr/ProfileBuilder.xaml.cs
@@ -6,4 +6,41 @@ public partial class ProfileBuilder : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ImageButton_Clicked(object sender, EventArgs e)
+    {
+        string result = await DisplayActionSheet("Pick a Photo", "Cancel", null, "fish_candy.png", "wheezer.jpg", "joker.jpg", "tyrunt.jpg", "pickel.png");
+        result = String.Concat("Resources/Images/Profile/", result);
+        PictureChanger.Source = result;
+       // VisualStateManager.GoToState(PictureChanger, "Pressed");
+    }
+
+    private void PictureChanger_Released(object sender, EventArgs e)
+    {
+        PictureChanger.Animate("Pressed", new Animation((d) =>
+        {
+            PictureChanger.Scale = d;
+        }, 0.9, 1, Easing.SpringOut));
+    }
+
+    private void PictureChanger_Focused(object sender, FocusEventArgs e)
+    {
+        VisualStateManager.GoToState(PictureChanger, "Pressed");
+
+    }
+
+    private void PictureChanger_Pressed(object sender, EventArgs e)
+    {
+        PictureChanger.Animate("Pressed", new Animation((d) =>
+        {
+            PictureChanger.Scale = d;
+        }, 1, 0.9, Easing.SpringOut));
+
+    }
+
+    private void PictureChanger_Unfocused(object sender, FocusEventArgs e)
+    {
+        VisualStateManager.GoToState(PictureChanger, "Normal");
+
+    }
 }
